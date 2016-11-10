f8c276411f6bd505e2194396ee92bd10ed9f8030
Mio Engel <mioengel@gmail.com>
2016-11-10 22:52:55 +0100

    renamed config and changed db name and username

--- src/main/java/com/valhallagame/instance_handling/Main.java
+++ src/main/java/com/valhallagame/instance_handling/Main.java
@@ -1,13 +1,10 @@
 package com.valhallagame.instance_handling;
 
-import org.skife.jdbi.v2.DBI;
-
 import com.valhallagame.instance_handling.configuration.InstanceHandlingConfiguration;
 import com.valhallagame.instance_handling.healthcheck.TemplateHealthCheck;
 import com.valhallagame.instance_handling.rest.InstanceResource;
 
 import io.dropwizard.Application;
-import io.dropwizard.jdbi.DBIFactory;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
@@ -29,8 +26,9 @@ public class Main extends Application<InstanceHandlingConfiguration> {
 	
 	@Override
 	public void run(InstanceHandlingConfiguration configuration, Environment environment) {
-		final DBIFactory factory = new DBIFactory();
-		final DBI jdbi = factory.build(environment, configuration.getDatabase(), "postgresql");
+		// This is to setup DAO but we don't have any yet
+		// final DBIFactory factory = new DBIFactory();
+		// final DBI jdbi = factory.build(environment, configuration.getDatabase(), "postgresql");
 		
 		final InstanceResource instanceResource = new InstanceResource();
 		
@@ -41,6 +39,6 @@ public class Main extends Application<InstanceHandlingConfiguration> {
 	}
 	
 	public static void start() throws Exception {
-		new Main().run("server", "hello-world.yml");
+		new Main().run("server", "instance-handling.yml");
 	}
 }
