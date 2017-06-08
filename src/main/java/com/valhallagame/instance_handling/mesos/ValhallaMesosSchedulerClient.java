45777cbc8de320764db68a9021682f835db6f771
Mikael Berglund <mikbe@kth.se>
2017-06-08 19:52:16 +0200

    More debug logging

--- src/main/java/com/valhallagame/instance_handling/mesos/ValhallaMesosSchedulerClient.java
+++ src/main/java/com/valhallagame/instance_handling/mesos/ValhallaMesosSchedulerClient.java
@@ -337,8 +337,9 @@ public class ValhallaMesosSchedulerClient extends MesosSchedulerClient {
             Response response = client.newCall(request).execute();
 
             if (response.code() != 200) {
+                log.error("Message was: " + message);
                 log.error("Something went wrong on instance update to persistent, code: " + response.code()
-                        + " " + response.body().string() + " with request " + request.toString());
+                        + " " + response.body().string() + " with request " + request.toString() + ": " + request.body().toString());
             }
         } catch (IOException e) {
             log.error("Something when wrong when trying to update persistent", e);
